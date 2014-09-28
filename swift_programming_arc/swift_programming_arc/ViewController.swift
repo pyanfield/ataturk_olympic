4749d8b8987e9ee50b104587663959b861d94ab1
pyanfield <pyanfield@gmail.com>
2014-09-28 12:47:28 +0800

    unowned reference

--- swift_programming_arc/swift_programming_arc/ViewController.swift
+++ swift_programming_arc/swift_programming_arc/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     var john: Person?
     var number73: Apartment?
     
+    var kaka: Customer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,6 +27,13 @@ class ViewController: UIViewController {
         // 当你把这两个变量设为nil时，两个析构函数被调用。打破了强应用循环
         john = nil
         number73 = nil
+        
+        // 无主引用的实例
+        kaka = Customer(name: "John Appleseed")
+        kaka!.card = CreditCard(number: 1234_5678_9012_3456, customer: kaka!)
+        
+        //
+        kaka = nil
     }
 }
 
@@ -45,6 +54,13 @@ class ViewController: UIViewController {
 // 弱引用必须被声明为变量，表明其值能在运行时被修改。弱引用不能被声明为常量。
 // 因为弱引用可以没有值，你必须将每一个弱引用声明为可选类型。
 
+// 和弱引用类似，无主引用不会牢牢保持住引用的实例。
+// 和弱引用不同的是，无主引用是永远有值的。因此，无主引用总是被定义为非可选类型（non-optional type）。
+// 你可以在声明属性或者变量时，在前面加上关键字 unowned 表示这是一个无主引用。
+// 无主引用总是可以被直接访问。不过 ARC 无法在实例被销毁后将无主引用设为nil，因为非可选类型的变量不允许被赋值为nil。
+
+// 如果你试图在实例被销毁后，访问该实例的无主引用，会触发运行时错误。使用无主引用，你必须确保引用始终指向一个未销毁的实例。
+// 还需要注意的是如果你试图访问实例已经被销毁的无主引用，程序会直接崩溃，而不会发生无法预期的行为。所以你应当避免这样的事情发生
 
 
 
@@ -61,4 +77,26 @@ class Apartment {
     // 声明成弱引用
     weak var tenant: Person?
     deinit { println("Apartment #\(number) is being deinitialized") }
+}
+
+// 无主引用，用以避免循环强引用
+class Customer {
+    let name: String
+    // 可能会有信用卡
+    var card: CreditCard?
+    init(name: String) {
+        self.name = name
+    }
+    deinit { println("\(name) is being deinitialized") }
+}
+
+class CreditCard {
+    let number: Int
+    // 但是每个信用卡肯定会有一个客户，一旦设置，将不能成为 nil
+    unowned let customer: Customer
+    init(number: Int, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    deinit { println("Card #\(number) is being deinitialized") }
 }
