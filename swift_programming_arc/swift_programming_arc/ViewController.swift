feb0d74c35b0c858c28ba77dc2784475f28bd546
pyanfield <pyanfield@gmail.com>
2014-09-28 11:33:03 +0800

    Strong Reference Cycles Between Class Instances

--- swift_programming_arc/swift_programming_arc/ViewController.swift
+++ swift_programming_arc/swift_programming_arc/ViewController.swift
@@ -10,34 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var reference1: Person?
-    var reference2: Person?
-    var reference3: Person?
+    var john: Person?
+    var number73: Apartment?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        reference1 = Person(name: "John Appleseed")
-        reference2 = reference1
-        reference3 = reference1
-        // 现在这个Person实例已经有三个强引用了。
-        reference1 = nil
-        reference2 = nil
-        // 通过给两个变量赋值nil的方式断开两个强引用（）包括最先的那个强引用），只留下一个强引用，Person实例不会被销毁
         
-        reference3 = nil
-        // 此时可以销毁 Person 实例了
+        john = Person(name: "John Appleseed")
+        number73 = Apartment(number: 73)
+        
+        // 想成循环强引用
+        john!.apartment = number73
+        number73!.tenant = john
+        
+        // 当你把这两个变量设为nil时，没有任何一个析构函数被调用。
+        // 强引用循环阻止了Person和Apartment类实例的销毁，并在你的应用程序中造成了内存泄漏。
+        john = nil
+        number73 = nil
     }
 }
 
 // 2.16 ARC
 // 引用计数仅仅应用于类的实例。结构体和枚举类型是值类型，不是引用类型，也不是通过引用的方式存储和传递。
 
+// 在两个类实例互相保持对方的强引用，并让对方不被销毁。这就是所谓的循环强引用。
+
 class Person {
     let name: String
-    init(name: String) {
-        self.name = name
-        println("\(name) >> is being initialized")
-    }
-    deinit {
-        println("\(name) >> is being deinitialized")
-    }
+    init(name: String) { self.name = name }
+    var apartment: Apartment?
+    deinit { println("\(name) is being deinitialized") }
+}
+
+class Apartment {
+    let number: Int
+    init(number: Int) { self.number = number }
+    var tenant: Person?
+    deinit { println("Apartment #\(number) is being deinitialized") }
 }
