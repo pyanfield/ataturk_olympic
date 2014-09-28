f8905bbdb5b12f9446cfa5565312e0e233fff8ec
pyanfield <pyanfield@gmail.com>
2014-09-28 11:54:25 +0800

    weak reference

--- swift_programming_arc/swift_programming_arc/ViewController.swift
+++ swift_programming_arc/swift_programming_arc/ViewController.swift
@@ -19,12 +19,10 @@ class ViewController: UIViewController {
         john = Person(name: "John Appleseed")
         number73 = Apartment(number: 73)
         
-        // 想成循环强引用
         john!.apartment = number73
         number73!.tenant = john
         
-        // 当你把这两个变量设为nil时，没有任何一个析构函数被调用。
-        // 强引用循环阻止了Person和Apartment类实例的销毁，并在你的应用程序中造成了内存泄漏。
+        // 当你把这两个变量设为nil时，两个析构函数被调用。打破了强应用循环
         john = nil
         number73 = nil
     }
@@ -34,6 +32,21 @@ class ViewController: UIViewController {
 // 引用计数仅仅应用于类的实例。结构体和枚举类型是值类型，不是引用类型，也不是通过引用的方式存储和传递。
 
 // 在两个类实例互相保持对方的强引用，并让对方不被销毁。这就是所谓的循环强引用。
+
+// Swift 提供了两种办法用来解决你在使用类的属性时所遇到的循环强引用问题：弱引用（weak reference）和无主引用（unowned reference）。
+// 弱引用和无主引用允许循环引用中的一个实例引用另外一个实例而不保持强引用。这样实例能够互相引用而不产生循环强引用。
+// 对于生命周期中会变为nil的实例使用弱引用。相反的，对于初始化赋值后再也不会被赋值为nil的实例，使用无主引用。
+
+// 弱引用不会牢牢保持住引用的实例，并且不会阻止 ARC 销毁被引用的实例。这种行为阻止了引用变为循环强引用。
+// 声明属性或者变量时，在前面加上weak关键字表明这是一个弱引用。
+
+// 在实例的生命周期中，如果某些时候引用没有值，那么弱引用可以阻止循环强引用。如果引用总是有值，则可以使用无主引用
+
+// 弱引用必须被声明为变量，表明其值能在运行时被修改。弱引用不能被声明为常量。
+// 因为弱引用可以没有值，你必须将每一个弱引用声明为可选类型。
+
+
+
 
 class Person {
     let name: String
@@ -45,6 +58,7 @@ class Person {
 class Apartment {
     let number: Int
     init(number: Int) { self.number = number }
-    var tenant: Person?
+    // 声明成弱引用
+    weak var tenant: Person?
     deinit { println("Apartment #\(number) is being deinitialized") }
 }
